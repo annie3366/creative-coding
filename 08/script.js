c9a183759bd599530f898a112a7927657a5c2f0c
Annie Hua <huaa162@newschool.edu>
2018-10-22 19:48:08 -0400

    new

--- 08/script.js
+++ 08/script.js
@@ -1,11 +1,33 @@
 const messageWrapper = document.getElementById('message');
 
-const name = prompt('3+29=');
-const number = prompt('29-3=');
-const value = prompt('3x29=');
+const add = prompt('3+29=');
+const subtract = prompt('32-3=');
+const multiply = prompt('29x3=');
+const equation = prompt('(3+29-3)x3')
 
-let message = '3+29=' + name + '!\n';
-message += '29-3=' + number + '!\n';
-message += '3x29= ' + value + '!\n'; 
+let message = '3+29=' + add + '!\n';
+if (add > 32) {
+    message += 'Wrong answer!\n';
+} else if (add < 32) {
+    message += 'Wrong answer!\n';
+}
+message += '32-3=' + subtract + '!\n';
+if (subtract > 29) {
+    message += 'Wrong answer!\n';
+} else if (subtract < 29) {
+    message += 'Wrong answer!\n';
+}
+message += '29x3= ' + multiply + '!\n'; 
+if (multiply > 87) {
+    message += 'Wrong answer!\n';
+} else if (multiply < 87) {
+    message += 'Wrong answer!\n';
+}
+message += '(3+29-3)x3=' + equation + '!\n';
+if (multiply > 87) {
+    message += 'Wrong answer!\n';
+} else if (multiply < 87) {
+    message += 'Wrong answer!\n';
+}
 
 messageWrapper.innerText = message;
