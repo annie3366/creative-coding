d43180542c8d67d51877f2d69a5d6c2d4b490591
Annie Hua <huaa162@newschool.edu>
2018-10-23 10:06:00 -0400

    new

--- 08/script.js
+++ 08/script.js
@@ -1,33 +1,12 @@
 const messageWrapper = document.getElementById('message');
 
-const add = prompt('3+29=');
-const subtract = prompt('32-3=');
-const multiply = prompt('29x3=');
-const equation = prompt('(3+29-3)x3')
+const one = parseInt(prompt('Enter first number'));
+const two = parseInt(prompt('Enter second number to add'));
+const three = parseInt(prompt('Enter third number to subtract'));
+const four = parseInt(prompt('Enter fourth number to multiply'));
 
-let message = '3+29=' + add + '!\n';
-if (add > 32) {
-    message += 'Wrong answer!\n';
-} else if (add < 32) {
-    message += 'Wrong answer!\n';
-}
-message += '32-3=' + subtract + '!\n';
-if (subtract > 29) {
-    message += 'Wrong answer!\n';
-} else if (subtract < 29) {
-    message += 'Wrong answer!\n';
-}
-message += '29x3= ' + multiply + '!\n'; 
-if (multiply > 87) {
-    message += 'Wrong answer!\n';
-} else if (multiply < 87) {
-    message += 'Wrong answer!\n';
-}
-message += '(3+29-3)x3=' + equation + '!\n';
-if (multiply > 87) {
-    message += 'Wrong answer!\n';
-} else if (multiply < 87) {
-    message += 'Wrong answer!\n';
-}
+const x= (one+two-three)*four
+
+let message = '(' + one + ' + ' + two + ' - ' + three + ') * ' + four + ' = ' + x;
 
 messageWrapper.innerText = message;
